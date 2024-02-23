5e7f5ede4d5bf176f380e38edf663830848c32b1
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-02-23 10:42:41 -0300

    remove unusual context

--- frontend/app/contexts/NavContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useState } from "react";
-
-export const NavContext = createContext(false);
-
-export const NavProvider = ({ children }: React.ReactNode) => {
-  return <NavContext.Provider>{children}</NavContext.Provider>;
-};
