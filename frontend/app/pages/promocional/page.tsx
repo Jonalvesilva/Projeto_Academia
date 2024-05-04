426af9afb7931c81d55019472ebab697743c566b
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-04 13:28:35 -0300

    feat: add masks and submit handler

--- frontend/app/pages/promocional/page.tsx
+++ frontend/app/pages/promocional/page.tsx
@@ -1,20 +1,82 @@
 "use client";
 import { unidades, controlNumber } from "@/app/utils/constants";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import { TextInputMask, cpfMask, celPhoneMask } from "react-masked-text";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserSchema, User } from "@/app/schemas/userSchema";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import { FaSpinner } from "react-icons/fa";
 
 export default function Promocional() {
   const {
     register,
-    control,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<User>({ resolver: zodResolver(UserSchema) });
 
-  const onSubmit: SubmitHandler<User> = (data) => {};
+  const handleCpf = (e: any) => {
+    let x = e.target.value
+      .replace(/\D/g, "")
+      .match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+    e.target.value = !x[2]
+      ? x[1]
+      : x[1] +
+        "." +
+        x[2] +
+        (x[3] ? "." : "") +
+        x[3] +
+        (x[4] ? "/" : x[4]) +
+        x[4] +
+        (x[5] ? "-" + x[5] : "");
+
+    if (e.target.value.length < 15) {
+      x = e.target.value
+        .replace(/\D/g, "")
+        .match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+      e.target.value = !x[2]
+        ? x[1]
+        : x[1] +
+          "." +
+          x[2] +
+          (x[3] ? "." : "") +
+          x[3] +
+          (x[4] ? "-" + x[4] : "");
+    }
+  };
+
+  const handlePhone = (event: any) => {
+    let input = event.target;
+    input.value = phoneMask(input.value);
+  };
+
+  const phoneMask = (value: any) => {
+    if (!value) return "";
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    return value;
+  };
+
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit: SubmitHandler<User> = (data) => {
+    setLoading(true);
+    delay(2000).then(() => {
+      Swal.fire({
+        title: "Excelente",
+        text: "Sua aula foi agendada!",
+        icon: "success",
+        confirmButtonText: "OK",
+      });
+      setLoading(false);
+      reset();
+    });
+  };
 
   return (
     <section className="border border-b-2 pt-[120px]">
@@ -65,18 +127,11 @@ export default function Promocional() {
                   CPF:
                 </label>
 
-                <TextInputMask
-                  mask={cpfMask()}
+                <input
                   className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  maxLength={14}
                   {...register("cpf")}
-                  onKeyDown={(event) => {
-                    if (
-                      !controlNumber.includes(event.key) ||
-                      event.key === "Dead"
-                    ) {
-                      event.preventDefault();
-                    }
-                  }}
+                  onInput={(e) => handleCpf(e)}
                 />
 
                 {errors.cpf && (
@@ -89,8 +144,7 @@ export default function Promocional() {
                 <label className="mb-3 block text-base font-medium text-[#07074D]">
                   Telefone
                 </label>
-                <TextInputMask
-                  mask={celPhoneMask()}
+                <input
                   className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   {...register("telefone")}
                   onKeyDown={(event) => {
@@ -101,6 +155,8 @@ export default function Promocional() {
                       event.preventDefault();
                     }
                   }}
+                  onKeyUp={(e) => handlePhone(e)}
+                  maxLength={15}
                 />
                 {errors.telefone && (
                   <span className="text-red-500">{`${errors.telefone.message}`}</span>
@@ -146,8 +202,12 @@ export default function Promocional() {
             </div>
           </div>
           <div className="w-full sm:w-[60%] mx-auto pt-6 pb-16">
-            <button className="rounded-md bg-green-600 py-3 w-full text-center text-base font-semibold text-white outline-none">
-              Marcar
+            <button className="rounded-md bg-green-600 py-3 w-full flex justify-center text-base font-semibold text-white outline-none">
+              {!loading ? (
+                "Marcar"
+              ) : (
+                <FaSpinner size={20} className="animate-spin" />
+              )}
             </button>
           </div>
         </form>
