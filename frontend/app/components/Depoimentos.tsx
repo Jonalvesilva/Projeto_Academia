dc3a6d1ab096301373cf7f4d227a7d4676af7f51
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-04 13:27:32 -0300

    feat: change bg url of hero and depoimentos

--- frontend/app/components/Depoimentos.tsx
+++ frontend/app/components/Depoimentos.tsx
@@ -12,7 +12,7 @@ import "swiper/css/navigation";
 export function Depoimentos() {
   return (
     <section
-      style={{ backgroundImage: "url('./planos.jpg')" }}
+      style={{ backgroundImage: "url('/planos.jpg')" }}
       className="min-w-[320px] flex h-[500px] bg-cover"
     >
       <div className="flex max-w-screen-lg items-center justify-center flex-col w-full lg:mx-auto gap-y-6">
