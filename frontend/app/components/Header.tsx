f1729e4e8b80b9a2148125b4136a2541b7c5f81b
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-20 10:41:00 -0300

    feat: add link navigation

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -4,6 +4,7 @@ import { FiMenu } from "react-icons/fi";
 import { linksHeader } from "../utils/constants";
 import { useEffect, useState } from "react";
 import SideNavbar from "./SideNavbar";
+import Link from "next/link";
 
 export default function Header() {
   const [nav, setNav] = useState(false);
@@ -22,19 +23,21 @@ export default function Header() {
       className="bg-emerald-900 h-16 fixed top-0 left-0 w-full z-[999] min-w-[320px]"
     >
       <nav className="h-full flex px-4 xl:px-0 items-center justify-between max-w-screen-xl mx-auto">
-        <div className="flex items-center gap-2 justify-center h-full">
-          <Image src={`/logo.png`} height={30} width={65} alt="logo" />
-          <span className="text-white text-xl sm:text-2xl md:text-3xl">
-            Academia do Jon
-          </span>
-        </div>
+        <Link href="/">
+          <div className="flex items-center gap-2 justify-center h-full">
+            <Image src={`/logo.png`} height={30} width={65} alt="logo" />
+            <span className="text-white text-xl sm:text-2xl md:text-3xl">
+              Academia do Jon
+            </span>
+          </div>
+        </Link>
         <div className="flex gap-10 items-center">
           <ul className="hidden lg:flex md:gap-10">
             {linksHeader.map((data) => {
               return (
-                <li key={data.name} className="text-white">
+                <Link href={data.url} key={data.name} className="text-white">
                   {data.name}
-                </li>
+                </Link>
               );
             })}
           </ul>
