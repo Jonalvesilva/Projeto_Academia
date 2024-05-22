06ad173359b084eb23b9c8f0de7774d873b67969
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-22 16:16:49 -0300

    feat: add sobre page

---
+++ frontend/app/pages/sobre/page.tsx
@@ -0,0 +1,12 @@
+export default function Sobre() {
+  return (
+    <section className="w-full border border-b-2 h-[700px] pt-[100px]">
+      <div className="py-12 max-w-screen-xl mx-auto">
+        <div className="text-3xl text-center">
+          <h2>Sobre Nós</h2>
+        </div>
+        <div className="flex flex-col max-w-screen-lg mx-auto items-center w-[90%] py-14"></div>
+      </div>
+    </section>
+  );
+}
