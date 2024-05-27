d718aef94017285d04fded693807323f1658d6ef
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-27 11:35:41 -0300

    feat: create treinos page

---
+++ frontend/app/pages/cliente/treinos/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Cliente() {
+  useEffect(() => {
+    document.getElementsByTagName("header")[0].classList.add("hidden");
+    document.getElementsByTagName("footer")[0].classList.add("hidden");
+  }, []);
+
+  return <>teste2</>;
+}
