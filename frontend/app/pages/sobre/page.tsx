0aaee1c38e01c28d3587d0ea0859ed28439f7653
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-10-28 14:24:19 -0300

    feat: modify page sobre

--- frontend/app/pages/sobre/page.tsx
+++ frontend/app/pages/sobre/page.tsx
@@ -1,11 +1,35 @@
 export default function Sobre() {
   return (
-    <section className="w-full border border-b-2 h-[700px] pt-[100px]">
+    <section className="w-full border border-b-2  pt-[100px]">
       <div className="py-12 max-w-screen-xl mx-auto">
         <div className="text-3xl text-center">
           <h2>Sobre Nós</h2>
         </div>
-        <div className="flex flex-col max-w-screen-lg mx-auto items-center w-[90%] py-14"></div>
+        <div className="flex flex-col max-w-screen-lg mx-auto items-center w-[90%] py-14 gap-y-12">
+          <p className="text-justify">
+            A Academia do Jon é um espaço dedicado ao bem-estar e à saúde, onde
+            pessoas de todas as idades e níveis de condicionamento físico podem
+            se reunir para alcançar seus objetivos. Com uma infraestrutura
+            moderna, a academia oferece uma variedade de equipamentos de
+            musculação, cardio e áreas para práticas de grupo, como yoga e
+            dança.
+          </p>
+          <p className="text-justify">
+            Os instrutores são altamente qualificados e estão sempre prontos
+            para ajudar os alunos a desenvolverem treinos personalizados, além
+            de oferecer orientações sobre nutrição e estilo de vida saudável. O
+            ambiente é acolhedor e motivador, promovendo um senso de comunidade
+            entre os frequentadores.
+          </p>
+          <p className="text-justify">
+            Além das aulas e treinos individuais, a Academia do Jon
+            frequentemente organiza eventos especiais, como desafios de fitness,
+            workshops e palestras sobre saúde, tornando a experiência ainda mais
+            enriquecedora. Se você está buscando melhorar sua forma física ou
+            apenas deseja se manter ativo, a Academia do Jon é o lugar ideal
+            para transformar seus objetivos em realidade.
+          </p>
+        </div>
       </div>
     </section>
   );
